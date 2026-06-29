18474ff15a2f9211ce0dbce846cd2408f02d9e2e
Akshat Pasbola <D24126231@mytudublin.ie>
2026-06-29 02:11:33 +0100

    added question explanation to subarraysumequalk problem

--- medium/prefix-sum/560_subarraysumequalsK.py
+++ medium/prefix-sum/560_subarraysumequalsK.py
@@ -1,8 +1,15 @@
+"""
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+"""
+
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         seen = {0:1}
         count = 0
-
+        prefix = 0
+        
         for item in nums:
             prefix = prefix + item
             count += seen.get(prefix - k, 0)
